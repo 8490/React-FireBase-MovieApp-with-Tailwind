4473877788e334b4a3d88558e0dec854e8059d37
8490 <ethan@clarusway.com>
2022-11-22 12:15:22 +0300

    add VideoSection

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import VideoSection from "../components/VideoSection";
 
 const MovieDetail = () => {
   const [movieDetails, setMovieDetails] = useState("");
+  const [videoKey, setVideoKey] = useState("");
   const { id } = useParams();
 
   const {
@@ -15,7 +17,6 @@ const MovieDetail = () => {
     release_date,
   } = movieDetails;
 
-  console.log(id);
   const API_KEY = process.env.REACT_APP_TMDB_KEY;
   const movieDetailBaseUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
   const baseImageUrl = "https://image.tmdb.org/t/p/w1280";
@@ -24,15 +25,20 @@ const MovieDetail = () => {
   const videoUrl = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`;
 
   useEffect(() => {
+    // Promise.all();
     axios
       .get(movieDetailBaseUrl)
       .then((res) => setMovieDetails(res.data))
       .catch((err) => console.log(err));
-  }, [movieDetailBaseUrl]);
+    axios
+      .get(videoUrl)
+      .then((res) => setVideoKey(res.data.results[0].key))
+      .catch((err) => console.log(err));
+  }, [movieDetailBaseUrl, videoUrl]);
   return (
     <div className="container px-10 mx-auto py-5">
       <h1 className="text-center text-white text-3xl">{title}</h1>
-      {/* {videoKey && <VideoSection videoKey={videoKey} />} */}
+      {videoKey && <VideoSection videoKey={videoKey} />}
       <div className="container flex justify-center px-10">
         <div className="flex flex-col lg:flex-row max-w-6xl rounded-lg bg-gray-100 shadow-lg">
           <img
